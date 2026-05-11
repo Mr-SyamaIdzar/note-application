cc5ae9e203186af0120a7f909789b5672a1d6afa
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2026-05-11 14:18:20 +0700

    fix: remove ssl parameter from prisma database url

--- backend/prisma/client.js
+++ backend/prisma/client.js
@@ -7,7 +7,7 @@ let databaseUrl = process.env.DATABASE_URL;
 // Jika Cloud Run menyediakan variabel DB_*, kita rangkai menjadi DATABASE_URL
 if (process.env.DB_HOST && process.env.DB_NAME && process.env.DB_USER) {
   const dbPass = process.env.DB_PASS || '';
-  databaseUrl = `mysql://${process.env.DB_USER}:${dbPass}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}?sslaccept=accept_invalid_certs`;
+  databaseUrl = `mysql://${process.env.DB_USER}:${dbPass}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`;
 }
 
 const prisma = new PrismaClient({
